642a9660ab2fcae978c8fa7609f199566228c150
Teshome Mulugeta <teshome_dagne@yahoo-com>
2025-11-03 10:18:02 -0800

    Initialize from extension template

--- jupytercon2025_extension_workshop/routes.py
+++ jupytercon2025_extension_workshop/routes.py
@@ -23,7 +23,7 @@ def setup_route_handlers(web_app):
     host_pattern = ".*$"
     base_url = web_app.settings["base_url"]
 
-    hello_route_pattern = url_path_join(base_url, "jupytercon2025-extension-workshop", "hello")
+    hello_route_pattern = url_path_join(base_url, "jupytercon2025-extension-workshop", "hello Teshome")
     handlers = [(hello_route_pattern, HelloRouteHandler)]
 
     web_app.add_handlers(host_pattern, handlers)
